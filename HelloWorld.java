3d69c15a0a373003035f207df894a9a1700e8af5
chasehoward85 <chasehoward85@gmail.com>
2018-01-08 00:21:32 -0700

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class Tester {
+
+	public static void main(String[] args) {
+		/*Programmer: Chase Howard
+		 Date Created: January 6th
+		 School: RMHS*/
+		
+		System.out.print("Hello World");
+		System.out.println("Hello Again");
+
+	}
+
+}
